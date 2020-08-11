13e6511f9ce5d5d79074a95526562648cc120d98
uihyeon.cha <uihyeon.cha@gmail.com>
2020-08-12 00:36:14 +0900

    Option 수정

--- src/option.ts
+++ src/option.ts
@@ -1,26 +1,30 @@
 import { Instance } from '~/instance.ts'
 
+export namespace Option {
+    export type Style = Partial<CSSStyleDeclaration>
+}
+
 // TODO: 미완성
 export class Option {
-    #parent: Option = null
-    #childs = new Set<Instance>()
-    #styles = new Set<Option>()
-    #attrs = new Set<Option>()
+    #parent: Instance = null
+    #childs: Instance[] = []
+    #styles = new Set<Option.Style>()
+    #attrs = new Map<string, string | number>()
 
-    get parent(): Option {
+    get parent(): Instance {
         return this.#parent
     }
-    set parent(parent: Option) {
+    set parent(parent: Instance) {
         // TODO: Instance가 아니면 실행 거부
         this.#parent = parent
     }
-    get childs(): Set<Instance> {
+    get childs(): Instance[] {
         return this.#childs
     }
-    get styles(): Set<Option> {
+    get styles(): Set<Option.Style> {
         return this.#styles
     }
-    get attrs(): Set<Option> {
+    get attrs(): Map<string, string | number> {
         return this.#attrs
     }
 }
