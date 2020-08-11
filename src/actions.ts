817fa142db5bd6d183dc03522cbcf71783e20a68
uihyeon.cha <uihyeon.cha@gmail.com>
2020-08-12 00:41:28 +0900

    action 작성 중

--- src/actions.ts
+++ src/actions.ts
@@ -1,8 +1,29 @@
-// TODO: 미완성
-export const addStyle = () => {}
+import { Option } from '~/option.ts'
 
 // TODO: 미완성
-export const addAttr = () => {}
+export const addStyle = (style: Option.Style) => {
+    const option = new Option()
+
+    option.styles.add(style)
+
+    return option
+}
+
+// TODO: 미완성
+export const addAttr = (key: string, val: string) => {
+    const option = new Option()
+
+    if (key === 'style') {
+        console.warn('style은 추가할 수 없습니다. `addStyle`을 사용하세요.')
+        return option
+    }
+
+    // return Promise.resolve(option)
+    return option
+}
 
 // TODO: 미완성
 export const addChild = () => {}
+
+// TODO: 미완성
+export const when = (condition, whenTruthy, whenFalsy) => {}
